018f745ba79a17117a15366f8a34768bfdc24140
3ammoory <omar@DESKTOP-4VFMQPA.localdomain>
2020-08-06 13:53:39 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="smg-3ammoory",  # Replace with your own username
+    version="0.0.1",
+    author="3ammoory",
+    author_email="omarabobakr2973@gmail.com",
+    description="A migrations tool for postgresql databases with multiple schemas",
+    url="https://github.com/3ammoory/migrations",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
