b246ade5fa683d9de9b8f911eb07adfff9594c44
3ammoory <omar@DESKTOP-4VFMQPA.localdomain>
2020-08-12 21:28:18 +0100

    finished first version of the init command

--- smg/smg.py
+++ smg/smg.py
@@ -1,7 +1,8 @@
 import os
 import typer
 from dotenv import load_dotenv
-from .utils import new_project
+from .base import new_project
+from .utils import coro
 
 app = typer.Typer()
 env = os.environ
@@ -16,4 +17,6 @@ def read_config(env_path: str = None):
 
 
 @app.command()
-def init(dsn: str = env.get('DB_URL'), schemaTable: str = env.get('SCHEMA_TABLE'), schemaRow: str = env.get('SCHEMA_ROW')):
+@coro
+async def init(dsn: str = env.get('DB_URL'), schemaTable: str = env.get('SCHEMA_TABLE'), schemaRow: str = env.get('SCHEMA_ROW')):
+    await new_project(dsn, schemaTable, schemaRow)
