3056637bf8590b5bbf11ad260f321da0dad77098
3ammoory <omar@DESKTOP-4VFMQPA.localdomain>
2020-08-12 21:25:30 +0100

    started working on the cli itself

--- smg/smg.py
+++ smg/smg.py
@@ -1,1 +1,19 @@
+import os
 import typer
+from dotenv import load_dotenv
+from .utils import new_project
+
+app = typer.Typer()
+env = os.environ
+
+
+@app.callback()
+def read_config(env_path: str = None):
+    if env_path:
+        load_dotenv(env_path)
+    else:
+        load_dotenv()
+
+
+@app.command()
+def init(dsn: str = env.get('DB_URL'), schemaTable: str = env.get('SCHEMA_TABLE'), schemaRow: str = env.get('SCHEMA_ROW')):
