ad43759dd2933a0ba8725f1a40b84ee783665944
3ammoory <omar@DESKTOP-4VFMQPA.localdomain>
2020-08-15 14:03:28 +0100

    fixing

--- smg/smg.py
+++ smg/smg.py
@@ -13,7 +13,7 @@ def read_config(env_path: str = None):
     if env_path:
         load_dotenv(env_path)
     else:
-        load_dotenv('config.env')
+        load_dotenv(os.path.join(os.getcwd(), 'config.env'))
     global env
     env = os.environ
 
