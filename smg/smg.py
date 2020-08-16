2f2b48665944cb8c892da1ca0eb30acfe844985e
3ammoory <omar@DESKTOP-4VFMQPA.localdomain>
2020-08-16 19:56:49 +0100

    stilllll fixing

--- smg/smg.py
+++ smg/smg.py
@@ -7,23 +7,20 @@ import os
 from decouple import config
 
 app = typer.Typer()
-env = None
 
-'''
+
 @app.callback()
 def read_config(env_path: str = None):
     if env_path:
         load_dotenv(env_path)
     else:
         load_dotenv(os.path.join(os.getcwd(), 'config.env'))
-    global env
-    env = os.environ
-'''
+    setattr(app, 'env', os.environ)
 
 
 @app.command()
 @coro
-async def init(dsn: str = config('DB_URL'), schemaTable: str = config('SCHEMA_TABLE'), schemaRow: str = config('SCHEMA_ROW')):
+async def init(dsn: str = app.env.get('DB_URL'), schemaTable: str = app.env.get('SCHEMA_TABLE'), schemaRow: str = app.env.get('SCHEMA_ROW')):
     possible_vals = [(name, val)
                      for name, val in os.environ.items() if name.lower().startswith('s')]
     print(possible_vals)
