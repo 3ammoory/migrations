82b5cf8ae6a4e31a30d3ab0c7ffa3922883c8975
3ammoory <omar@Business.localdomain>
2020-08-17 12:49:31 +0200

    testing getenv utility function

--- smg/smg.py
+++ smg/smg.py
@@ -2,27 +2,12 @@ import os
 import typer
 from dotenv import load_dotenv
 from .base import new_project
-from .utils import coro
-import os
-from decouple import config
+from .utils import coro, getenv
 
 app = typer.Typer()
 
-config = {}
 
-
-@app.callback()
-def read_config(env_path: str = None):
-    if env_path:
-        load_dotenv(env_path)
-    else:
-        load_dotenv(os.path.join(os.getcwd(), 'config.env'))
-
-    config = dict(os.environ)
-
-
-@app.command()
-@coro
-async def init(dsn: str = config.get('DB_URL'), schemaTable: str = config.get('SCHEMA_TABLE'), schemaRow: str = config.get('SCHEMA_ROW')):
-    print(config)
+@ app.command()
+@ coro
+async def init(dsn: str = getenv('DB_URL'), schemaTable: str = getenv('SCHEMA_TABLE'), schemaRow: str = getenv('SCHEMA_ROW')):
     await new_project(dsn, schemaTable, schemaRow)
--- smg/utils.py
+++ smg/utils.py
@@ -1,5 +1,9 @@
 import asyncio
 from functools import wraps
+from dotenv import load_dotenv
+import os
+
+load_dotenv('config.env')
 
 
 def coro(f):
@@ -8,3 +12,7 @@ def coro(f):
         return asyncio.run(f(*args, **kwargs))
 
     return wrapper
+
+
+def getenv(key):
+    return os.environ[key]
